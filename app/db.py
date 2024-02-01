d8b6860b33066f937fde44cd70c39027860c3226
Gaspar Ortmann <gasparortmann@gmail.com>
2024-02-01 11:13:53 -0300

    DB and table files creation

--- app/db.py
+++ app/db.py
@@ -1,21 +1,20 @@
 import mysql.connector
 
-def connect_db(host,username, password, db_name):
+def connect_db(host,username, password):
     # Establece una conexión con la base de datos MySQL.
     try:
         conn = mysql.connector.connect(
             host=host,
             user=username,
             password=password,
-            database=db_name
+
         )
         print("Conexión exitosa!")
         return conn
     except mysql.connector.Error as err:
         print("Error: ", err)
 
-def create_db():
-        conn = connect_db('localhost', 'root', 'root', 'meli_test')
+def create_db(conn):
         # Crear la base de datos
         cursor = conn.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS drive_inventory_db")
@@ -36,4 +35,3 @@ def create_db():
 
         # Cerrar la conexión
         cursor.close()
-        conn.close()
--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-
 from app import db
 
-
-
 def main():
-    conn = db.connect_db('localhost', 'root', 'root', 'meli_test')
-    db.create_db()
 
+
+    # Conexión a la base de datos MySQL
+    host = "localhost"
+    username = "root"
+    password = "root"
+    database_name = "drive_inventory_db"
+
+    conn = db.connect_db(host, username, password)
+
+    # Crear la tabla de archivos si no existe
+    db.create_db(conn)
+
+   
+    # Cerrar conexión a la base de datos
+    conn.close()
 
 if __name__ == '__main__':
     main()
